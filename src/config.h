5813f74cb7a4372565904bd38f2085dcb94858bc
Trunone <trunone@ubuntu.(none)>
2014-09-13 07:32:09 -0700

    Fix to build in Ubuntu 12.04

---
+++ src/config.h
@@ -0,0 +1,106 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+/* src/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if translation of program messages to the user's native
+   language is requested. */
+#define ENABLE_NLS 1
+
+/* Compile as floating-point */
+#define FLOATING_POINT /**/
+
+/* Define to 1 if you have the MacOS X function CFLocaleCopyCurrent in the
+   CoreFoundation framework. */
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+
+/* Define to 1 if you have the MacOS X function CFPreferencesCopyAppValue in
+   the CoreFoundation framework. */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+
+/* Define if the GNU dcgettext() function is already present or preinstalled.
+   */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Use gtkmm */
+#define HAVE_GTKMM 1
+
+/* Define if you have the iconv() function and it works. */
+/* #undef HAVE_ICONV */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `portaudio' library (-lportaudio). */
+#define HAVE_LIBPORTAUDIO 1
+
+/* Define to 1 if you have the `portmixer' library (-lportmixer). */
+/* #undef HAVE_LIBPORTMIXER */
+
+/* Define to 1 if you have the `sndfile' library (-lsndfile). */
+#define HAVE_LIBSNDFILE 1
+
+/* Define to 1 if you have the `speexdsp' library (-lspeexdsp). */
+#define HAVE_LIBSPEEXDSP 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "openhriaudio"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "yosuke.matsusaka@aist.go.jp"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "OpenHRIAudio"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "OpenHRIAudio 2.01"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "openhriaudio"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.01"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Use FFT from OggVorbis */
+#define USE_SMALLFT /**/
+
+/* Version number of package */
+#define VERSION "2.01"
